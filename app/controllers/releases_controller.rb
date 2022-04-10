bf56f257a99b4961ec4e1252db60a2360c201434
vaizu <vaizu07204869@yahoo.co.jp>
2022-04-10 15:55:13 +0000

    Release_create

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -7,16 +7,22 @@ class ReleasesController < ApplicationController
   def create
     @release = Release.new(release_params)
     if @release.save
-      redirect_to root_path
+      redirect_to release_path(@release)
     else
       render :new
     end
   end
 
+  def show
+    @release = Release.find(params[:id])
+
+    @sum = 0
+  end
+
   private
 
   def release_params
-    params.require(:release).permit(:sub_title, :main_title, :release_day, :detail,
+    params.require(:release).permit(:sub_title, :main_title, :release_day, :release_image, :detail,
                                   musics_attributes:[:id, :release_id, :music_title, :lyrics, :composition, :_destroy])
   end
 end
